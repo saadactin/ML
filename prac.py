4e71e4fce648d80910561aa1e78fbba9f51832eb
Saad Sayyed <saad.sayyed@actin.co.in>
2025-12-09 10:02:09 +0530

    day 6

--- prac.py
+++ prac.py
@@ -1,6 +1,37 @@
-numbers = [1, 3, 7, 2, 9, 4]
+# numbers = [1, 3, 7, 2, 9, 4]
 
-for num in numbers:
-    if num > 5:
-        print(f"Found: {num}")
-        break
+# for num in numbers:
+#     if num > 5:
+#         print(f"Found: {num}")
+#         break
+
+
+
+# shape
+ans2 = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+
+])
+# print(ans2.shape) 
+#  shape =  (rows,columns)
+# print(ans2.size) // total number of elements 
+# print(ans2.ndim) // number of dimensions 
+# print(ans2.dtype) // data type of the elements 
+# print(ans2.astype(float)) // convert the array to any data type 
+
+# // fast math operations  without using loops
+
+# print(np.sum(ans2))
+# print(np.mean(ans2))
+# print(ans2 * 2)
+# print(ans2 + 2)
+
+
+# Aggregation functioms 
+
+# sum , mean , max ,min 
+
+# print(np.std(ans2))
+# print(np.var(ans2))
